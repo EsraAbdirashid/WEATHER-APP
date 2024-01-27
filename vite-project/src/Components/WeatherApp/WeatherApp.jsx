59bb30fa5864c635b818d969b6bbe870fffcbbc2
EsraAbdirashid <israApdirashid@gmail.com>
2024-01-27 06:01:15 +0300

    Done

--- vite-project/src/Components/WeatherApp/WeatherApp.jsx
+++ vite-project/src/Components/WeatherApp/WeatherApp.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './WeatherApp.css'
 
-// import search_icon from '../../assets/search.png';
+import search_icon from '../../assets/search.png';
 // import clear_icon from '../../assets/clear.png';
 // import cloud_icon from '../../assets/cloud.png';
 // import drizzle_icon from '../../assets/drizzle.jpg';
