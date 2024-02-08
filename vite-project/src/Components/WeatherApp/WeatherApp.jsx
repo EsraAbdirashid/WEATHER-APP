2b82dc09a76a889642fc242ff570b8bb65c0a61b
EsraAbdirashid <israApdirashid@gmail.com>
2024-02-08 17:53:44 +0300

    Done

--- vite-project/src/Components/WeatherApp/WeatherApp.jsx
+++ vite-project/src/Components/WeatherApp/WeatherApp.jsx
@@ -4,11 +4,11 @@ import './WeatherApp.css'
 import search_icon from "../Assets/search.png";
 import clear_icon from "../Assets/clear.png";
 import cloud_icon from "../Assets/cloud.png";
-import drizzle_icon from "../Assets/
-import rain_icon from "../Assets/
-import snow_icon from "../Assets/
-import wind_icon from "../Assets/
-import humidity_icon from "../Assets/
+import drizzle_icon from "../Assets/drizzle";
+import rain_icon from "../Assets/rain.jpg";
+import snow_icon from "../Assets/snow.jpg";
+import wind_icon from "../Assets/wind.jpg";
+import humidity_icon from "../Assets/humidity.png";
 
 const WeatherApp = () => {
   return (
