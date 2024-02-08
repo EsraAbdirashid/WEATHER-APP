0d9d07c9092cd40a5a5d987fad5839d7584cef70
EsraAbdirashid <israApdirashid@gmail.com>
2024-02-08 18:35:17 +0300

    Done

--- vite-project/src/Components/WeatherApp/WeatherApp.jsx
+++ vite-project/src/Components/WeatherApp/WeatherApp.jsx
@@ -5,10 +5,10 @@ import search_icon from '../../../public/search.png';
 import clear_icon from'../../../public/clear.png'; 
 import cloud_icon from '../../../public/cloud.png';
 import drizzle_icon from '../../../public/drizzle.jpg';
-import rain_icon from 
-import snow_icon from 
-import wind_icon from 
-import humidity_icon from
+import rain_icon from   '../../../public/rain.jpg';
+import snow_icon from  '../../../public/snow.jpg';
+import wind_icon from  '../../../public/wind.jpg';
+import humidity_icon from  '../../../public/humidity.png';
 
 const WeatherApp = () => {
   return (
@@ -17,7 +17,7 @@ const WeatherApp = () => {
       <div className="top-bar">
         <input type="text" className='cityInput' placeholder='search' />
         <div className="search-icon">
-        <img src={search_icon} alt="img" />
+        <img src={search_icon} alt="img" className='w-6 h-6' />
         </div>
       </div>
      </div>
